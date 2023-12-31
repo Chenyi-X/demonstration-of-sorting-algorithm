bc06c9ae60885062263d9bea6099367501300457
Chenyi <971984463@qq.com>
2023-12-31 18:19:06 +0800

    utf-8

--- include/Stack.h
+++ include/Stack.h
@@ -1,11 +1,11 @@
 #pragma once
 typedef int ElemType;
 typedef struct {
-	ElemType* elem;     // 存储空间的基址
-	int top;    // 栈顶元素的下一个位置，简称栈顶位标
-	int size;    // 当前分配的存储容量
-	int increment;    // 扩容时，增加的存储容量
-} SqStack;         // 顺序栈
+	ElemType* elem;     
+	int top;    
+	int size;    
+	int increment;    
+} SqStack;        
 
 #define OVERFLOW -1
 #define OK 1
@@ -13,8 +13,8 @@ typedef struct {
 #define TRUE 2
 #define FALSE -2
 
-#define INITSIZE 5   //栈的初始容量
-#define INCREMENT 2   //栈扩容时的增量
+#define INITSIZE 5  
+#define INCREMENT 2   
 
 typedef int Status;
 
